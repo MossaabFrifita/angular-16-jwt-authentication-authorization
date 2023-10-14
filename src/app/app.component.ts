77c8cae6e129ce85507ec7588e3696db676c5ed8
MossaabFrifita <frifita1@gmail.com>
2023-10-14 15:11:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./_services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-auth';
+
+  constructor(private authService : AuthService) {
+  }
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
+
 }
